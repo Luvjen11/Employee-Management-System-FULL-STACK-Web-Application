207c8534c69eecf0a93b1be6d80d686672d1fcaa
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-03-04 13:29:29 +0000

    create footer component

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import ListEmployee from './components/ListEmployee'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <>
       <Header />
       <ListEmployee />
-
+      <Footer />
     </>
   )
 }
--- ems-frontend/src/components/Header.jsx
+++ ems-frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div>
       <header>
         <nav className='navbar navbar-dark bg-dark'>
-            <a class="navbar-brand mb-0 h1">Employee Management System</a>
+            <a className="navbar-brand mb-0 h1">Employee Management System</a>
         </nav>
       </header>
     </div>
