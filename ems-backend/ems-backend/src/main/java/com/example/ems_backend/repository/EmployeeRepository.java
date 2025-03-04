60127a656b96a853f3fb78390618f731a75ef2e9
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-03-04 10:21:53 +0000

    create employee repository interface

--- ems-backend/ems-backend/src/main/java/com/example/ems_backend/repository/EmployeeRepository.java
+++ ems-backend/ems-backend/src/main/java/com/example/ems_backend/repository/EmployeeRepository.java
@@ -1,5 +1,11 @@
 package com.example.ems_backend.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ems_backend.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     
 }
