2c35eeadd24ac23e01cea0ae32b56be9932a56ea
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-03-04 12:56:19 +0000

    install axios and create employee service

---
+++ ems-frontend/src/services/EmployeeService.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+// step 1: create a base URL
+const REST_API_BASE_URL = 'http://localhost:8080/api/employees';
+
+
+// step 2: create an arrow function that will return the data from the API
+// step 3: export the function
+// get all employees
+export const listEmployees = () => axios.get(REST_API_BASE_URL);
+
